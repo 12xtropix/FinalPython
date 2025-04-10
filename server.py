55db39bcc7c45f1a6f9070bee00b816f47a3e0d6
bmuhlendorf <bmuhlendorf@potomacschool.org>
2025-04-10 11:33:58 -0400

    c&p fixed thing

--- server.py
+++ server.py
@@ -8,8 +8,8 @@ socketio = SocketIO(app)
 # Game data
 players = []
 prompts = []
-faker_prompt = []
-player_sockets = {}  # username -> socket ID
+player_sockets = {}
+ready_players = set()
 
 game_data = {
     "rounds": 4,
@@ -18,21 +18,17 @@ game_data = {
     "player_role": {},  # Mapping player -> role (faker or normal)
 }
 
-
 @app.route('/')
 def index():
     return render_template('mainscreen.html')
-
 
 @app.route('/host')
 def host():
     return render_template('hostscreen.html')
 
-
 @app.route('/player')
 def player():
     return render_template('playerscreen.html')
-
 
 @app.route('/play', methods=['POST'])
 def play():
@@ -47,17 +43,14 @@ def play():
 
     return jsonify({"status": response})
 
-
 def handle_answer(player_name):
     if game_data["player_role"].get(player_name) == "faker":
-        return faker_prompt
+        return "You're the faker! Try to blend in."
     else:
         return random.choice(prompts)
 
-
 def handle_vote(player_name):
     return "Vote recorded!"
-
 
 def generate_prompt_sequence():
     types = ["hands", "pointing", "numbers"]
@@ -82,7 +75,6 @@ def generate_prompt_sequence():
 
     return selected_prompts
 
-
 @socketio.on('join_game')
 def handle_join_game(data):
     username = data.get('username')
@@ -98,13 +90,12 @@ def handle_join_game(data):
         emit('player_joined', {'message': f'{username} has joined the game!'}, broadcast=True)
         emit('players_update', {'players': players}, broadcast=True)
 
-
 @socketio.on('start_game')
 def handle_start_game():
-    global prompts
+    global prompts, ready_players
+    ready_players = set()
     prompts = generate_prompt_sequence()
 
-    # Assign roles (1 faker, rest normal)
     faker = random.choice(players)
     game_data["player_role"] = {
         player: ("faker" if player == faker else "normal") for player in players
@@ -112,6 +103,8 @@ def handle_start_game():
 
     current_prompt = prompts[game_data["current_round"] - 1]
     prompt_type = current_prompt.split('_')[0]
+    prompt_text_for_normal = current_prompt
+    prompt_text_for_faker = "You're the faker! Try to blend in."
 
     for player in players:
         role = game_data["player_role"].get(player)
@@ -119,24 +112,39 @@ def handle_start_game():
         if not sid:
             continue
 
-        if role == "faker":
-            prompt_text = "You are the faker. Try to blend in!"
-        else:
-            prompt_text = current_prompt
+        prompt_text = prompt_text_for_normal if role != "faker" else prompt_text_for_faker
 
-        socketio.emit(
-            'serve_prompt',
-            {"prompt_type": prompt_type, "prompt_text": prompt_text},
-            room=sid
-        )
+        socketio.emit('serve_prompt', {
+            "prompt_type": prompt_type,
+            "prompt_text": prompt_text
+        }, room=sid)
 
-    socketio.emit('start_countdown', {"seconds": 20})
+    # Send prompt to host so it's revealed after countdown or all ready
+    socketio.emit('host_prompt', {
+        "prompt_type": prompt_type,
+        "prompt_text": prompt_text_for_normal
+    })
 
+@socketio.on('player_ready')
+def handle_player_ready(data):
+    username = data.get('username')
+    if username and username in players:
+        ready_players.add(username)
+        emit('ready_update', {
+            "ready_count": len(ready_players),
+            "total_players": len(players)
+        }, broadcast=True)
+
+        if len(ready_players) == len(players):
+            # All ready, skip countdown
+            socketio.emit('start_countdown', {"seconds": 0})
+        elif len(ready_players) == 1:
+            # First player ready, start regular countdown
+            socketio.emit('start_countdown', {"seconds": 20})
 
 @socketio.on('connect')
 def handle_connect():
     emit('players_update', {'players': players})
-
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
